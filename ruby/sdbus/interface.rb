ef0d3b1e8fb8ca0692bb995d3d0258800c62533d
skinnyjames <zero@skinnyjames.net>
2026-06-06 08:23:10 -0400

    feat(interface): adds SDBus::Interface#set

--- ruby/sdbus/interface.rb
+++ ruby/sdbus/interface.rb
@@ -21,6 +21,24 @@ module SDBus
       @meta[:signals]
     end
 
+    def set(prop_name, value)
+      if prop = props[prop_name]
+        raise "Cannot get a writable prop" unless prop[:access] == "write" || prop[:access] == "readwrite"
+
+        vargs = [prop[:type], value]
+        args = [name, prop_name, vargs]
+
+        conn.call(
+          object.service.name,
+          object.name,
+          "org.freedesktop.DBus.Properties",
+          "Set",
+          "ssv",
+          args
+        )
+      end
+    end
+
     def get(prop_name)
       if prop = props[prop_name]
         raise "Cannot get a writable prop" unless prop[:access] == "read" || prop[:access] == "readwrite"
